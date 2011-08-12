cc9e63adeed7fede735355e468b484c2f401adc0
Torsten Rüger <torsten@lightning.nu>
2011-08-13 00:26:47 +0300

    creating the order just before printing (no dead orders)

--- app/controllers/admin/pos_controller.rb
+++ app/controllers/admin/pos_controller.rb
@@ -3,36 +3,38 @@ class Admin::PosController < Admin::BaseController
   
 
   def new
-    session[:order] = nil
+    session[:order] = []
     redirect_to :action => :index
   end
 
   def print
-    unless @order.shipment
-      @order.shipping_method = ShippingMethod.find_by_name "nouto"
-      @order.create_shipment!
+    order = Order.new
+    order.email = current_user.email
+    order.save!
+    @items.each do |idd , price |
+      var = Variant.find(idd)
+      puts "Variant #{var.name} #{idd}"
+      new_item = LineItem.new(:quantity => 1 )
+      new_item.variant = var
+      new_item.price = price.to_s 
+      order.line_items << new_item
     end
-    unless @order.payment
-      TaxRate.all.each do |rate|
-        rate.create_adjustment( rate.tax_category.description , @order, @order, true)
-      end 
-      payment = Payment.new( :payment_method => PaymentMethod.find_by_type( "PaymentMethod::Check") , 
-                :amount => @order.total , :order_id => @order.id )
-      payment.complete
-      payment.save!
+    order.shipping_method = ShippingMethod.find_by_name "nouto"
+    order.create_shipment!
+    TaxRate.all.each do |rate|
+      rate.create_adjustment( rate.tax_category.description , order, order, true)
     end
-    unless @order.completed?
-      @order.state = "complete"
-      @order.completed_at = Time.now
-    end
-    redirect_to "/admin/invoice/#{@order.number}/receipt"
+    payment = Payment.new( :payment_method => PaymentMethod.find_by_type( "PaymentMethod::Check") , 
+              :amount => order.total , :order_id => order.id )
+    payment.complete
+    payment.save!
+    order.state = "complete"
+    order.completed_at = Time.now
+    order.save!
+    redirect_to "/admin/invoice/#{order.number}/receipt"
   end
   
   def index
-    unless session[:order]
-      @order.save!
-      session[:order] = @order.id 
-    end
     puts "PROD #{@products.length}"
     
     if pid = params[:add]
@@ -40,7 +42,7 @@ class Admin::PosController < Admin::BaseController
       flash.notice = t('product_added')
     end
     if pid = params[:remove]
-      remove_product Product.find pid
+      remove_product pid
       flash.notice = t('product_removed')
     end
     render :index
@@ -64,24 +66,12 @@ class Admin::PosController < Admin::BaseController
   private
 
   def add_product prod
-    new_item = LineItem.new(:quantity => 1)
     var = prod.class == Product ? prod.master : prod 
-    new_item.variant = var
-    new_item.price   = var.price
-    @order.line_items << new_item
-    @order.save!
+    @items << [ var.id , var.price ]
+    session[:items] = @items
   end
   def load
-    begin
-      @order = Order.find session[:order]
-      @order.shipping_method = ShippingMethod.find_by_name "nouto"
-    rescue
-      session[:order] = nil
-    ensure
-      @order = Order.new unless @order
-    end
-    @order.email = current_user.email
-    @empty = params[:search] == nil
+    @items = session[:items] || []
     init_search
   end
   
--- app/helpers/admin/pos_helper.rb
+++ app/helpers/admin/pos_helper.rb
@@ -4,29 +4,11 @@ module Admin::PosHelper
     "/admin/pos/find"
   end
   
-  def source_link task
-    return " " unless task.source
-    case task.source_type 
-    when "Product"
-      link_to task.source.name , edit_admin_product_url(task.source)
-    when "Order"
-      link_to( "#{task.source.number} ( #{task.source.total} )" , admin_order_url(task.source)) 
-    else
-      task.source_id ?  task.source.to_s : ""
+  def item_total
+    sum = 0 
+    @items.each do |id , price|
+      sum += price
     end
-  end
-  
-  def done_link pos
-    if pos.done 
-      I18n.t('done') 
-    else
-      link_to I18n.t('to_do') , done_admin_pos_url(pos)
-    end
-  end
-  
-  def self.find_task(object)
-    return unless object
-    task = AdminTask.find_by_source_id(object.id)
-  end
-  
+    sum
+  end 
 end
