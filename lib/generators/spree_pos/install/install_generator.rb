ef38f842974e912399e3e0c44112bbbbe655ba6d
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-01-14 00:06:29 +0530

    updating images, removing css file, updating generator to not include both css and js files

--- lib/generators/spree_pos/install/install_generator.rb
+++ lib/generators/spree_pos/install/install_generator.rb
@@ -1,17 +1,6 @@
 module SpreePos
   module Generators
     class InstallGenerator < Rails::Generators::Base
-
-      def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_pos\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_pos\n"
-      end
-
-      def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_pos\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_pos\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_pos'
       end
