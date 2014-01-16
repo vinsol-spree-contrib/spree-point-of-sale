85a80d1f54b89a2de402c37d438821a7b2309158
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-01-16 15:08:42 +0530

    assign shipment on no shipment on remove, callback on shipment to assign billaddress and shipaddress for order on stock location change and validate no inventory unit for stock location change

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,4 +1,6 @@
 Spree::Shipment.class_eval do
+  before_save :udpate_order_addresses_from_stock_location, :if => ["order.is_pos?", "stock_location_id.present?", :stock_location_id_changed? ]
+  validate :empty_inventory, :if => ["order.is_pos?", "stock_location_id.present?", :stock_location_id_changed? ] 
 
   def finalize_pos
     self.state = "shipped"
@@ -12,5 +14,16 @@ Spree::Shipment.class_eval do
     shipment.stock_location = Spree::StockLocation.stores.active.first
     shipment.shipping_methods << Spree::ShippingMethod.where(:name => SpreePos::Config[:pos_shipping]).first
     shipment.save!
+  end
+
+  private
+
+  def empty_inventory
+    errors[:base] = "Inventory Units assigned for the order. Please remove them to change stock location" if inventory_units.present? 
+  end
+
+  def udpate_order_addresses_from_stock_location
+    order.bill_address = order.ship_address = stock_location.address
+    order.save
   end
 end
