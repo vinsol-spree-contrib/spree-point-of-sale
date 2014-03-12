ec97e8d15fc07f93ee5cc1d504028210a28b2d7d
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-03-12 16:47:16 +0530

    change arguments to be used same as in spree for co ordinator build packages

--- app/models/spree/stock/coordinator_decorator.rb
+++ app/models/spree/stock/coordinator_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Stock::Coordinator.class_eval do
   #overwrite the spree method to not use stores to build packages
-  def build_packages(packages)
+  def build_packages(packages = Array.new)
     ::Spree::StockLocation.active.not_store.each do |stock_location|
       packer = build_packer(stock_location, order)
       packages += packer.packages
